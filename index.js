efdf6ff8a3314ebf33da303710777ed100f5f072
SaeedNagi <saeednagi42@gmail.com>
2025-08-24 16:14:26 +0500

    Updated code

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ btn.onclick = ()=>{
       const li = document.createElement("li");
       li.innerHTML = searchinput.value;
 
+
       const completeBtn = document.createElement("span");
       completeBtn.innerHTML = " ✔ ";
       completeBtn.style.cursor = "pointer";
-      completeBtn.style.marginLeft = "70px";
+      completeBtn.style.color = "White";
+      completeBtn.style.borderRadius = "50%";
+      completeBtn.style.width = "50vw"
+      completeBtn.style.height = "50vh"
+      completeBtn.style.marginLeft = "15vw";
+      completeBtn.style.fontSize = "2rem"
+      completeBtn.style.backgroundColor = " Black";
       completeBtn.onclick = () => {
         li.style.textDecoration = "line-through";
         li.style.color = "gray";
@@ -18,8 +25,14 @@ btn.onclick = ()=>{
 
         const deleteBtn = document.createElement("span");
         deleteBtn.innerHTML = " ✘ ";
+        deleteBtn.style.color = "White";
+        deleteBtn.style.backgroundColor = " Black";
+        deleteBtn.style.borderRadius = "50%";
+        deleteBtn.style.width = "50vw"
+        deleteBtn.style.fontSize = "2rem"
+        deleteBtn.style.height = "50vh"
         deleteBtn.style.cursor = "pointer";
-         deleteBtn.style.marginLeft = "70px";
+        deleteBtn.style.marginLeft = "15vw";
         deleteBtn.onclick = () => {
         li.remove();
     };
